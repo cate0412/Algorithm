16b8cf99e6e85cdcecfb112d9d70a35237a9514b
cate0412 <114456602+cate0412@users.noreply.github.com>
2023-01-30 19:22:11 +0900

    [Bronze II] Title: 16진수, Time: 108 ms, Memory: 12784 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main (String args[]){
+      Scanner sc = new Scanner(System.in);
+
+      String num = sc.next();
+        int decimal = Integer.parseInt(num,16);
+        System.out.println(decimal);
+        
+    }
+}
